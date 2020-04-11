a6fe8653107ce8ad642b57081eefe562c3d3ce24
Gothicvx <55742147+Gothicvx@users.noreply.github.com>
2020-04-11 14:27:25 +0200

    Khorinis

---
+++ ZamekNew/Assets/Skok/Skrypty/Door2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+       animator = GetComponent <Animator>();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open", false);
+        }
+    }
+}
+
